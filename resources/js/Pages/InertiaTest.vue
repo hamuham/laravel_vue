17cf182d5818d0b844d08a775816c0c159d8e876
hamuham <uni.uni.yui@hotmail.co.jp>
2024-02-05 20:38:55 +0900

    inertiaTest

--- resources/js/Pages/InertiaTest.vue
+++ resources/js/Pages/InertiaTest.vue
@@ -7,5 +7,6 @@ import { Link } from '@inertiajs/inertia-vue3';
 InertiaTest<br>
 <a href="/">aタグ</a><br>
 <Link href="/">Linkタグ</Link><br>
+<Link :href="route('inertia.index')">ineriaroutetest</Link>
 
 </template>
