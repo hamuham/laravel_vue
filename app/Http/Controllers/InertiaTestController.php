4058671876807e2d50de33f7c2038223392b42e1
hamuham <uni.uni.yui@hotmail.co.jp>
2024-04-18 09:19:52 +0900

    validation

--- app/Http/Controllers/InertiaTestController.php
+++ app/Http/Controllers/InertiaTestController.php
@@ -38,6 +38,9 @@ class InertiaTestController extends Controller
         $inertiaTest->content = $request->content;
         $inertiaTest->save();
 
-        return to_route('inertia.index');
+        return to_route('inertia.index')
+        ->with([
+            'message' => '登録完了'
+        ]);
     }
 }
--- resources/js/Pages/Inertia/Index.vue
+++ resources/js/Pages/Inertia/Index.vue
@@ -1,4 +1,7 @@
 <script setup></script>
 <template>
+    <div v-if="$page.props.flash.message">
+    {{ $page.props.flash.message }}
+    </div>
 てsと
 </template>
