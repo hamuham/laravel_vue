5ae93bb1864d83f776081e7c46a210156805e6cc
hamuham <uni.uni.yui@hotmail.co.jp>
2024-05-03 16:40:58 +0900

    emit

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,18 @@
+<script setup>
+import GuestLayout from '@/Layouts/Guest.vue'
+import Lavel from '@/Component/Label.vue'
+import Lavel from '@/Component/Input.vue'
+
+const emitTest = e => console.log(e)
+</script>
+
+<template>
+<GuestLayout>
+    <Label value="件名">タイトル</Label>
+    <Input
+        modelValue="初期値"
+        @emitTest="emitTest"
+        ></Input>
+</GuestLayout>
+
+</template>
