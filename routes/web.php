d073afc80feaae1b9604b66ca5670b431786aa8e
hamuham <uni.uni.yui@hotmail.co.jp>
2024-05-21 23:32:43 +0900

    dummyData

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,15 @@ use Inertia\Inertia;
 use App\Http\Controllers\InertiaTestController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controlers\PurchaseControler;
 
 Route::resource('items', ItemController::class)
 ->middleware(['auth', 'verified']);
 
 Route::resource('customers', CustomerController::class)
+->middleware(['auth', 'verified']);
+
+Route:resource('purchases', PurchaseControler:class)
 ->middleware(['auth', 'verified']);
 
 Route::get('/inertia-test', function () {
