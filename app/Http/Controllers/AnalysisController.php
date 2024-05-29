0816fbcd78c268f327100fa4486d0353958047a8
hamuham <uni.uni.yui@hotmail.co.jp>
2024-05-30 00:53:35 +0900

    vue-chart-3

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Inertia\Inertia;
 use App\Models\Order;
+use Illuminate\Support\Facades\DB;
 
 class AnalysisController extends Controller
 {
@@ -15,12 +18,23 @@ class AnalysisController extends Controller
         $startDate = '2022-08-20';
         $endDate = '2022-08-21';
 
-        $period = Order::betweenDate($startDate, $endDate)
+        // $period = Order::betweenDate($startDate, $endDate)
+        // ->groupBy('id')
+        // ->selectRaw('id, sum(subtotal) as total,
+        // customer_name, status, created_at')
+        // ->orderBy('created_at')
+        // ->paginate(50);
+
+        $subQuery = Order::betweenDate($startDate, $endDate)
+        ->where('status', true)
         ->groupBy('id')
-        ->selectRaw('id, sum(subtotal) as total,
-        customer_name, status, created_at')
-        ->orderBy('created_at')
-        ->paginate(50);
+        ->selectRaw('id, sum(subtotal) as totalPerPurchase,
+        DATE_FORMAT(created_at, "%Y%m%d") as date');
+
+        $data = DB::table($subQuery)
+        ->groupBy('date')
+        ->selectRaw('date, sum(totalPerPurchase) as total' )
+        ->get();
 
         return Inertia::render('analysis');
     }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Scopes\Subtotal;
+use Carbon\Carbon;
 
 class Order extends Model
 {
